64fdcb67aa3edcd5915db8f8fe89d4b5f4dd70de
Odekunle Jibola Johnson <45737516+Ratselaft@users.noreply.github.com>
2019-09-18 20:00:24 +0800

    Update Dreamteam.php

--- Dreamteam.php
+++ Dreamteam.php
@@ -1,7 +1,46 @@
-<? php
-function Login(){
-	if (empty($_POST['username'])){
-		echo "username is required";
-	}
+<? PHP
+define('USER', 'root');
+define('PASSWORD', '');
+define('HOST', 'localhost');
+define('DATABASE', 'test');
+
+try {
+$connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);}
+catch (PDOException $e){
+exit("Error: " . $e->getMessage());}
+
+CREATE TABLE `users` ( `id` int(10) unsigned NOT NULL AUTO_INCREMENT, `username` varchar(25) NOT NULL,`password` varchar(255) NOT NULL,`email` varchar(100) NOT NULL, PRIMARY KEY (`id`), UNIQUE KEY `username` (`username`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+include('config.php');
+session_start();
+ 
+if (isset($_POST['register'])) {
+ 
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+ 
+    $query = $connection->prepare("SELECT * FROM users WHERE EMAIL=:email");
+    $query->bindParam("email", $email, PDO::PARAM_STR);
+    $query->execute();
+ 
+    if ($query->rowCount() > 0) {
+        echo '<p class="error">The email address is already registered!</p>';
+    }
+ 
+    if ($query->rowCount() == 0) {
+        $query = $connection->prepare("INSERT INTO users(USERNAME,PASSWORD,EMAIL) VALUES (:username,:password_hash,:email)");
+        $query->bindParam("username", $username, PDO::PARAM_STR);
+        $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
+        $query->bindParam("email", $email, PDO::PARAM_STR);
+        $result = $query->execute();
+ 
+        if ($result) {
+            echo '<p class="success">Your registration was successful!</p>';
+        } else {
+            echo '<p class="error">Something went wrong!</p>';
+        }
+    }
 }
+
 ?>
